0b160c1b0a2b0f04a8fb099deababa061eb485ec
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-01 10:53:11 -0800

    good fail Rule1

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,5 +1,6 @@
 import { Lines } from './../src/haiku.js';
 import { Vowel } from './../src/haiku.js';
+import { Rule1 } from './../src/haiku.js';
 
 describe('Lines', () => {
 
@@ -14,6 +15,19 @@ describe('Vowel', () => {
   test('it should identify a vowel in a word', () => {
     expect(Vowel('cat')).toBe(true);
     expect(Vowel('rtp')).toBe(false);
+  });
+  
+});
+
+describe('Rule1', () => {
+
+  test('it should divide a word into syllabules following rule1', () => {
+    expect(Rule1('banana')).toBe(3);
+    expect(Rule1('children')).toBe(2);
+    expect(Rule1('the')).toBe(1);
+    expect(Rule1('tape')).toBe(1); //silent e is not counted as a vowel
+    expect(Rule1('loud')).toBe(1); //When two vowels carry one sound (diphthong), they cannot be divided
+    expect(Rule1('many')).toBe(2); //The letter “y” is not strictly a vowel but behaves like one.
   });
   
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,13 +1,25 @@
 
-
-export function Lines(input) {
-  function countInstances(string, word) {
+function countInstances(string, word) {
     return string.split(word).length - 1;
   }
+
+export function Lines(input) {
+
   if (countInstances(input, "\n") === 2) {
     return 3;
   }
 }
 
-export function Vowel(input) {
+export function Vowel(input)
+{
+    if (countInstances(input, 'a') === 0 && countInstances(input, 'e') === 0 && countInstances(input, 'i') === 0 && countInstances(input, 'o') === 0 && countInstances(input, 'u') === 0 && countInstances(input, 'u') === 0) {
+      return false;
+    }
+    else {
+      return true;
+    }
 }
+
+export function Rule1(input) {
+}
+
